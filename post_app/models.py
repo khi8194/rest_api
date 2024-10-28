90612271f52d554184786f3eecd7f33804c71447
khi8194 <khi8194@gmail.com>
2024-10-28 09:50:43 +0900

    게시글 저장을 위한 모델 생성

--- post_app/models.py
+++ post_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 
 class Post(models.Model):
   CATEGORY = (('STUDY','Study'),('DIARY','Diary'),('COMMON','Common')) #튜플형 카테고리
@@ -12,3 +14,24 @@ class Post(models.Model):
   # created & updated 처음에는 동일하게 생성
   created = models.DateTimeField(auto_now_add=True) #없던 모델을 새로 생성
   updated = models.DateTimeField(auto_now=True) #기존에 있던 모델중 업데이트 내용 날짜값 기록
+  
+  # Post 클래스에서 생성되는 인스턴스 모델의 타이틀 값을 문자열로 반환
+  def __str__(self): # 장고전용 instance객체
+    return self.title
+  
+  # 해당 클래스가 생성하는 모델 인스턴스로 저장해주는 함수
+  def save(self, *args, **kwargs):
+    # 모델 인스턴스 생성시 slug 항목이 없으면 제목값을 slug화 해서 대신 저장해주는 기능
+    if not self.slug:
+      slug_base = slugify(self.title)
+      slug = slug_base
+    
+    # 이미 DB상의 같은 항목의 slug 게시글이 존재하면 
+    # 기존 slug 이름에 랜덤한 5글자 고유 문자값을 적용하여 slug 중복피함  
+    if Post.objects.filter(slug=slug).exists():
+      #위의 조건문으로 중복회피한 새로운 슬러그를 인스턴스에 적용
+      slug = f'{slug_base}-{get_random_string(5)}'
+    self.slug = slugify
+    
+    #부모 클래스의 save함수를 super를 이용하여 위의 적용 내용들을 반환해서 호출 및 저장
+    super(Post,self).save(*args, **kwargs)
